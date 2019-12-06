57c098a2022a1ea6172abe51d605e2d9b0ccba37
elfri.app <elfri.app@gmail.com>
2019-12-06 15:45:06 +0700

    just layout, the rest of task i can finish this night , I'll try my best (because my laptop had been bluescreen several time)

--- App.js
+++ App.js
@@ -14,6 +14,8 @@ import {
   View,
   Text,
   StatusBar,
+  TouchableOpacity,
+  Dimensions,
 } from 'react-native';
 
 import {
@@ -23,92 +25,179 @@ import {
   DebugInstructions,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
+const sw = Math.round(Dimensions.get('window').width);
+const sh = Math.round(Dimensions.get('window').height);
+
+const onTap = (inp) => {
+
+  console.warn(inp)
+}
 
 const App: () => React$Node = () => {
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes wkwkwkwkwk</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One wiwiwiwiwii</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
+    <View style={styles.cont}>
+
+      {/* result */}
+      <View style={styles.res}>
+        <Text style={styles.resTxt}>15.000</Text>
+      </View>
+
+      {/* calculation */}
+      <View style={styles.calc}>
+        <Text style={styles.calcTxt}>
+          10.000 + 5.000
+        </Text>
+      </View>
+
+      {/* button  */}
+      <View style={styles.btns}>
+
+        <View style={styles.btnRow}>
+          <TouchableOpacity style={styles.btnItem} onPress={() => onTap('ac')} >
+            <Text style={[styles.btnTxt, { color: 'black' }]}>AC</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.btnItem} onPress={() => onTap('posNeg')} >
+            <Text style={[styles.btnTxt, { color: 'black' }]}>+/-</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.btnItem} onPress={() => onTap('percent')} >
+            <Text style={[styles.btnTxt, { color: 'black' }]}>%</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.btnItem} onPress={() => onTap('divider')} >
+            <Text style={[styles.btnTxt, styles.btnOpr2,]}>/</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.btnRow}>
+          <TouchableOpacity style={styles.btnItem} onPress={() => onTap(7)} >
+            <Text style={[styles.btnTxt, styles.btnNum]}>7</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.btnItem} onPress={() => onTap(8)} >
+            <Text style={[styles.btnTxt, styles.btnNum]}>8</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.btnItem} onPress={() => onTap(9)} >
+            <Text style={[styles.btnTxt, styles.btnNum]}>9</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.btnItem} onPress={() => onTap('multiplier')} >
+            <Text style={[styles.btnTxt, styles.btnOpr2]}>X</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.btnRow}>
+          <TouchableOpacity style={styles.btnItem} onPress={() => onTap(4)} >
+            <Text style={[styles.btnTxt, styles.btnNum]}>4</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.btnItem} onPress={() => onTap(5)} >
+            <Text style={[styles.btnTxt, styles.btnNum]}>5</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.btnItem} onPress={() => onTap(6)} >
+            <Text style={[styles.btnTxt, styles.btnNum]}>6</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.btnItem} onPress={() => onTap('minus')} >
+            <Text style={[styles.btnTxt, styles.btnOpr2]}>-</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.btnRow}>
+          <TouchableOpacity style={styles.btnItem} onPress={() => onTap(1)} >
+            <Text style={[styles.btnTxt, styles.btnNum]}>1</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.btnItem} onPress={() => onTap(2)} >
+            <Text style={[styles.btnTxt, styles.btnNum]}>2</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.btnItem} onPress={() => onTap(3)} >
+            <Text style={[styles.btnTxt, styles.btnNum]}>3</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.btnItem} onPress={() => onTap('plus')} >
+            <Text style={[styles.btnTxt, styles.btnOpr2]}>+</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.btnRow}>
+          <TouchableOpacity style={[styles.btnItem, { flex: 2 / 4, }]} onPress={() => onTap(0)} >
+            <Text style={[styles.btnTxt, { width: sw * .44 }, styles.btnNum]}>0</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.btnItem} onPress={() => onTap('comma')} >
+            <Text style={[styles.btnTxt, styles.btnNum]}>.</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.btnItem} onPress={() => onTap('equal')} >
+            <Text style={[styles.btnTxt, styles.btnOpr2]}>=</Text>
+          </TouchableOpacity>
+        </View>
+
+
+      </View>
+
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+
+  cont: {
+    flex: 1,
+    backgroundColor: 'black',
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
+  res: {
+    flex: 3 / 15,
+    justifyContent: 'flex-end',
+    alignItems: 'flex-end',
   },
-  body: {
-    backgroundColor: Colors.white,
+  calc: {
+    flex: 1 / 15,
+    backgroundColor: 'grey',
+    justifyContent: 'center',
+    alignItems: 'flex-end',
   },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  btns: {
+    flex: 11 / 15,
+    justifyContent: 'flex-start'
+    // backgroundColor: 'yellow',
+    // flexGrow: 7,
+    // flexDirection: 'row',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
+
+  resTxt: {
+    color: 'white',
+    fontSize: sw * .2,
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
+  calcTxt: {
+    color: 'white',
+    fontSize: sw * .1,
   },
-  highlight: {
-    fontWeight: '700',
+
+
+  btnCircle: {
+    flex: 1 / 4,
+    // width: 100,
+    // height: 100,
+    // borderRadius: 100 / 2,
+    backgroundColor: 'red'
   },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
+  btnRow: {
+    flex: 1, flexDirection: 'row'
   },
+  btnItem: {
+    // alignSelf: 'center',
+    // backgroundColor: 'red',
+    flex: 1 / 4,
+    padding: sw * .005
+  },
+  btnTxt: {
+    paddingTop: sw * .04,
+    width: sw * .21,
+    height: sw * .21,
+    backgroundColor: 'grey',
+    borderRadius: (sw * .21) / 2,
+    textAlign: 'center',
+    alignSelf: 'center',
+    // alignItems: 'center',
+    // justifyContent: 'center',
+    color: 'white',
+    fontSize: sw * .1
+  },
+  btnNum: {
+    backgroundColor: '#333333'
+  },
+  btnOpr2: {
+    backgroundColor: 'orange'
+  }
+
 });
 
 export default App;
